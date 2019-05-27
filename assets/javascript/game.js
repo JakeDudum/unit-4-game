fbe85b6f80df172ec16ebe7afd0cbef7aca2cd7f
Jake Dudum <jakedudum@gmail.com>
2019-05-26 17:17:17 -0700

    Created objects for all characters in the game and gave them required attributes in game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+
+var goku = {
+    name: "Goku",
+    hp: 100,
+    ap: 10,
+    cap: 10
+};
+
+var gohan = {
+    name: "Gohan",
+    hp: 100,
+    ap: 10,
+    cap: 10
+};
+
+var cell = {
+    name: "Cell",
+    hp: 100,
+    ap: 10,
+    cap: 10
+};
+
+var frieza = {
+    name: "Frieza",
+    hp: 100,
+    ap: 10,
+    cap: 10
+};
+
+var buu = {
+    name: "Majin Buu",
+    hp: 100,
+    ap: 10,
+    cap: 10
+};
+
+
+
+
+});
